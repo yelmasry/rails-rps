701e316b2433fb69445986d1ff44dc34ce9621e9
yelmasry <yarra.elmasry@gmail.com>
2025-01-26 23:58:21 +0000

    rails rock

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,47 @@
+class ZebraController < ApplicationController
+  def giraffe 
+    @random_move = ["rock", "paper", "scissors"].sample
+    if @random_move == "rock" 
+      @outcome = "tied" 
+    elsif @random_move == "paper" 
+      @outcome = "lost" 
+    elsif 
+      @outcome = "won" 
+    end 
+    render({ :template => "game_templates/play_rock" }) 
+  end 
+
+
+
+  def elephant 
+    @random_move = ["rock", "paper", "scissors"].sample 
+    if @random_move == "paper"
+      @outcome = "tied"
+    elsif @random_move == "rock" 
+      @outcome = "won"
+    elsif 
+      @outcome = "lost"
+    end
+    render({ :template => "game_templates/play_paper" })
+  end 
+
+
+
+  def lion 
+    @random_move = ["rock", "paper", "scissors"].sample 
+    if @random_move == "scissors"
+      @outcome = "tied"
+    elsif @random_move == "rock" 
+      @outcome = "lost"
+    elsif 
+      @outcome = "won"
+    end 
+    render({ :template => "game_templates/play_scissors" })
+  end 
+
+
+
+  def rules 
+    render({ :template => "game_templates/homepage" })
+  end 
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get("/rock", { :controller => "zebra", :action => "giraffe" })
 
+  get("/paper", { :controller => "zebra", :action => "elephant" })
+
+  get("/scissors", { :controller => "zebra", :action => "lion" })
+
+  get("/", { :controller => "zebra", :action => "rules" })
 end
